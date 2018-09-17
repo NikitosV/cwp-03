433a79e5a24a9a6df91d18e39a0ba7831471f229
Nikita <Niktos.V@tut.by>
2018-09-17 12:15:53 +0300

    rename client.js to client-qa.js

--- client-qa.js
+++ client-qa.js
@@ -1,4 +1,4 @@
-// client.js
+// client-qa.jsjs
 const net = require('net');
 const fs = require('fs');
 const port = 8124;
@@ -6,24 +6,24 @@ const string = 'QA';
 const bad = 'DEC';
 const good = 'ACK';
 
-const client = new net.Socket();
+const clientQa = new net.Socket();
 let currentIndex = -1;
-client.setEncoding('utf8');
+clientQa.setEncoding('utf8');
 
 let questions = [];
-client.connect({port: port, host: '127.0.0.1'}, () => {
+clientQa.connect({port: port, host: '127.0.0.1'}, () => {
     fs.readFile("qa.json", (err, text) => {
         if (!err) {
             questions = JSON.parse(text);
-            client.write(string);
+            clientQa.write(string);
         }
         else console.error(err);
     });
 });
 
-client.on('data', (data) => {
+clientQa.on('data', (data) => {
     if (data === bad)
-        client.destroy();
+        clientQa.destroy();
     if (data === good)
         sendQuestion();
     else {
@@ -37,15 +37,15 @@ client.on('data', (data) => {
     }
 });
 
-client.on('close', function () {
+clientQa.on('close', function () {
     console.log('Connection closed');
 });
 
 function sendQuestion() {
     if (currentIndex < questions.length-1) {
         let qst = questions[++currentIndex].quest;
-        client.write(qst);
+        clientQa.write(qst);
     }
     else
-        client.destroy();
+        clientQa.destroy();
 }
